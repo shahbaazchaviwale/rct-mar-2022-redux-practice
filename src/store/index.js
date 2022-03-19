2daacaa53d1bab9342a51474ae189a4471b649e0
Shahbaaz <shahbaazc@gmail.com>
2022-03-19 17:15:40 +0530

    created a redux store for react

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+// step-2
+const counterReducer = (state = {counter: 0}, action) => {
+    if(action.type === 'increment'){
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter: state.counter + 1
+        }
+    }
+    // this below  state will be unchange/default values
+    return state;
+}
+// step-1
+const store = createStore(counterReducer);
+
+export default store;
