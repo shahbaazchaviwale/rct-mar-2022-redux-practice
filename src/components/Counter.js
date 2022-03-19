ab6d4f894a9fe749fd632ce62448266ed46f599c
Shahbaaz <shahbaazc@gmail.com>
2022-03-19 17:17:39 +0530

    using redux data in react component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  const counter = useSelector(state => state.counter);
+  
   const toggleCounterHandler = () => {};
 
   return (
